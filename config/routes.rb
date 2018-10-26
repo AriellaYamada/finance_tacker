5f8adc3cb6607065d22385fe43f852ea2b2bea7c
AriellaYamada <ariella.y.b@hotmail.com>
2018-10-26 17:38:06 -0300

    adding delete user stock

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 	get 'my_portfolio', to: "users#my_portfolio"
 	get "search_stocks", to: "stocks#search"
 
-	resources :user_stocks, only: [:create]
+	resources :user_stocks, only: [:create, :destroy]
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
